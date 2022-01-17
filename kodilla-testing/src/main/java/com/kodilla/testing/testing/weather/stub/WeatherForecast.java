8dfbd0fc97e337ece171e6cec84ecb03d28a6dd2
kuba jarosz <kuba.jarooosz@gmail.com>
2022-01-17 21:11:13 +0100

    6.6 WeatherForecast average and median methods with tests

--- kodilla-testing/src/main/java/com/kodilla/testing/testing/weather/stub/WeatherForecast.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/testing/weather/stub/WeatherForecast.java
@@ -1,5 +1,7 @@
 package com.kodilla.testing.testing.weather.stub;
 
+import java.sql.Array;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -17,7 +19,44 @@ public class WeatherForecast {
                 temperatures.getTemperatures().entrySet()){
             resultMap.put(temperature.getKey(), temperature.getValue() + 1.0);
         }
-
         return resultMap;
+    }
+
+    public double countAverageTemperature(){
+        double sum = 0;
+
+        for(Map.Entry<String, Double> temperature: temperatures.getTemperatures().entrySet()){
+            sum += temperature.getValue();
+        }
+
+        int temp = (int)(sum/temperatures.getTemperatures().size()*100);
+        double result = temp/100.0;
+
+        return result;
+    }
+
+    public double medianTemperature(){
+        Map<String, Double> temperaturesMap = temperatures.getTemperatures();
+        int mapSize = temperaturesMap.size();
+        double [] temperaturesArray = new double [mapSize];
+        int index = 0;
+        double result;
+
+        for(Map.Entry<String, Double> temperature: temperaturesMap.entrySet()){
+            temperaturesArray[index] = temperature.getValue();
+            index++;
+        }
+
+        Arrays.sort(temperaturesArray);
+
+        if(mapSize % 2 == 0){
+            double sumOfMiddleElements = (temperaturesArray[mapSize/2]+
+                                temperaturesArray[mapSize/2-1]);
+            result = sumOfMiddleElements / 2;
+        }else{
+            result = temperaturesArray[mapSize/2];
+        }
+
+        return result;
     }
 }
--- kodilla-testing/src/test/java/com/kodilla/testing/weather/mock/WeatherForecastTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/weather/mock/WeatherForecastTestSuite.java
@@ -3,6 +3,8 @@ package com.kodilla.testing.weather.mock;
 import com.kodilla.testing.testing.weather.stub.Temperatures;
 import com.kodilla.testing.testing.weather.stub.WeatherForecast;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
@@ -19,9 +21,8 @@ public class WeatherForecastTestSuite {
     @Mock
     private Temperatures temperaturesMock;
 
-    @Test
-    void testCalculateForecastWithMock(){
-        //given
+    @BeforeEach
+    void createMap(){
         Map<String, Double> temperaturesMap = new HashMap<>();
         temperaturesMap.put("Wroclaw", 22.5);
         temperaturesMap.put("Rzeszow", 27.3);
@@ -29,6 +30,11 @@ public class WeatherForecastTestSuite {
         temperaturesMap.put("Szczecin", 23.8);
         temperaturesMap.put("Olsztyn", 20.1);
         when(temperaturesMock.getTemperatures()).thenReturn(temperaturesMap);
+    }
+
+    @Test
+    void testCalculateForecastWithMock(){
+        //given
         WeatherForecast weatherForecast = new WeatherForecast(temperaturesMock);
 
         //when
@@ -37,4 +43,29 @@ public class WeatherForecastTestSuite {
         //then
         Assertions.assertEquals(5, quantityOfSensors);
     }
+
+    @Test
+    void testCalculateAverageTemperature(){
+        //given
+        WeatherForecast weatherForecast = new WeatherForecast(temperaturesMock);
+
+        //when
+        double result = weatherForecast.countAverageTemperature();
+
+        //then
+        Assertions.assertEquals(22.72, result);
+    }
+
+    @Test
+    void testMedianTemperature(){
+        //given
+        WeatherForecast weatherForecast = new WeatherForecast(temperaturesMock);
+
+        //when
+        double result = weatherForecast.medianTemperature();
+
+        //then
+        Assertions.assertEquals(22.5, result);
+    }
+
 }
