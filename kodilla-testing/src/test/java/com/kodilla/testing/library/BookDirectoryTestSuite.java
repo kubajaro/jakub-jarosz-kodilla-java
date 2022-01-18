50e88185d5bba0f7022e142d5c59bc2264c6e407
kuba jarosz <kuba.jarooosz@gmail.com>
2022-01-18 21:57:06 +0100

    bookd directory class tests draft - TDD

---
+++ kodilla-testing/src/test/java/com/kodilla/testing/library/BookDirectoryTestSuite.java
@@ -0,0 +1,21 @@
+package com.kodilla.testing.library;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class BookDirectoryTestSuite {
+
+    @Test
+    void testListBooksWithConditionsReturnList(){
+        assertTrue(false);
+    }
+
+    void testListBooksWithConditionsMoreThan20(){
+        assertTrue(false);
+    }
+
+    void testListBooksWithConditionsFragmentShorterThan3(){
+        assertTrue(false);
+    }
+}
