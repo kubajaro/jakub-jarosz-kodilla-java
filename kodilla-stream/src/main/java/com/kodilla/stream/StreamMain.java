469dd2d25109c6e7f6b062b7dd5fc5ebced47567
kuba jarosz <kuba.jarooosz@gmail.com>
2022-01-21 00:06:03 +0100

    stream - lambda function

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,7 +1,13 @@
 package com.kodilla.stream;
 
+import com.kodilla.stream.lambda.Executor;
+import com.kodilla.stream.lambda.Processor;
+import com.kodilla.stream.lambda.SaySomething;
+
 public class StreamMain {
     public static void main(String[] args) {
-
+        Processor processor = new Processor();
+        Executor codeToExecute = () -> System.out.println("Execute");
+        processor.execute(codeToExecute);
     }
 }
