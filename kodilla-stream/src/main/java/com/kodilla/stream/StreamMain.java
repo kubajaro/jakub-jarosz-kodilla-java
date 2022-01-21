1023c3529201f1d5c0808f0c851e3c173c9b21e0
kuba jarosz <kuba.jarooosz@gmail.com>
2022-01-21 19:19:57 +0100

    lambda expression

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,13 +1,17 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.lambda.Executor;
+import com.kodilla.stream.lambda.ExpressionExecutor;
 import com.kodilla.stream.lambda.Processor;
 import com.kodilla.stream.lambda.SaySomething;
 
 public class StreamMain {
     public static void main(String[] args) {
-        Processor processor = new Processor();
-        Executor codeToExecute = () -> System.out.println("Execute");
-        processor.execute(codeToExecute);
+        ExpressionExecutor ex = new ExpressionExecutor();
+
+        ex.executeExpression(2,3, (a,b) -> a + b);
+        ex.executeExpression(4,4, (a, b) -> a * b);
+        ex.executeExpression(5, 2, (a, b) -> a - b);
+        ex.executeExpression(4, 2, (a, b) -> a / b);
     }
 }
