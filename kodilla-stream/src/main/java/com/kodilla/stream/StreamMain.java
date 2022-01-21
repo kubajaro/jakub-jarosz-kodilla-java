748e3910197be9500648c8349653a555b668608e
kuba jarosz <kuba.jarooosz@gmail.com>
2022-01-21 19:55:36 +0100

    lambda expression - functional method

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -4,6 +4,7 @@ import com.kodilla.stream.lambda.Executor;
 import com.kodilla.stream.lambda.ExpressionExecutor;
 import com.kodilla.stream.lambda.Processor;
 import com.kodilla.stream.lambda.SaySomething;
+import com.kodilla.stream.reference.FunctionalCalculator;
 
 public class StreamMain {
     public static void main(String[] args) {
@@ -13,5 +14,11 @@ public class StreamMain {
         ex.executeExpression(4,4, (a, b) -> a * b);
         ex.executeExpression(5, 2, (a, b) -> a - b);
         ex.executeExpression(4, 2, (a, b) -> a / b);
+
+        System.out.println("----Method references");
+        ex.executeExpression(2,3, FunctionalCalculator::addAToB);
+        ex.executeExpression(4,4, FunctionalCalculator::multiplyAByB);
+        ex.executeExpression(5, 2, FunctionalCalculator::subBFromA);
+        ex.executeExpression(4,2, FunctionalCalculator::divideAByB);
     }
 }
