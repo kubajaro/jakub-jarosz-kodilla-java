d12cad9b00e400df7efec1a941d1167a9e4b6b1b
kuba jarosz <kuba.jarooosz@gmail.com>
2022-01-22 23:56:52 +0100

    7.3 kolektor Collectors.toList()

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,14 +1,22 @@
 package com.kodilla.stream;
 
+import com.kodilla.stream.book.Book;
 import com.kodilla.stream.book.BookDirectory;
 import com.kodilla.stream.person.People;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class StreamMain {
     public static void main(String[] args) {
         BookDirectory theBookDirectory = new BookDirectory();
 
-        theBookDirectory.getList().stream()
+        List<Book> theResult = theBookDirectory.getList().stream()
                 .filter(book -> book.getYearOfPublication() > 2005)
+                .collect(Collectors.toList());
+
+        System.out.println("# elements: " + theResult.size());
+        theResult.stream()
                 .forEach(System.out::println);
     }
 }
