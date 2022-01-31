9affe443ea6d9662ad6791830d253b170a91a4de
kuba jarosz <kuba.jarooosz@gmail.com>
2022-01-31 21:50:27 +0100

    8.2 try catch - file reader refactored

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/main/ExceptionModuleRunner.java
@@ -0,0 +1,12 @@
+package com.kodilla.exception.main;
+
+import com.kodilla.exception.io.FileReader;
+
+import java.io.File;
+
+public class ExceptionModuleRunner {
+    public static void main(String[] args) {
+        FileReader fileReader = new FileReader();
+        fileReader.readFile();
+    }
+}
