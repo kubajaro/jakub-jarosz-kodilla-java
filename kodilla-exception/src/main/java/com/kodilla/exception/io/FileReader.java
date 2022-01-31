e3b8369d101e085b5fa24c2c74eac099bf1e12cb
kuba jarosz <kuba.jarooosz@gmail.com>
2022-01-31 22:01:14 +0100

    8.2 try catch - file reader refactored - finally

--- kodilla-exception/src/main/java/com/kodilla/exception/io/FileReader.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/io/FileReader.java
@@ -11,12 +11,13 @@ public class FileReader {
     public void readFile(){
         ClassLoader classLoader = getClass().getClassLoader();
         File file = new File(classLoader.getResource("names.txt").getFile());
-        Path path = Paths.get(file.getPath());
 
         try(Stream<String> fileLines = Files.lines(Paths.get(file.getPath()))) {
             fileLines.forEach(System.out::println);
         }catch(IOException e){
             System.out.println("Mamy błąd " + e);
+        }finally {
+            System.out.println("To jest element finally bloku try-catch-finally");
         }
         System.out.println(file.getPath());
     }
