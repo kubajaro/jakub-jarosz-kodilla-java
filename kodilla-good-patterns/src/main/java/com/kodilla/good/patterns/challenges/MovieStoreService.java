8279f5ca471810a959b2194b46d3d30844498141
kuba jarosz <kuba.jarooosz@gmail.com>
2022-03-04 18:31:07 +0100

    13.1 Map stream iteration fixed

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/MovieStoreService.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/MovieStoreService.java
@@ -2,11 +2,14 @@ package com.kodilla.good.patterns.challenges;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class MovieStoreService {
 
     public static void printMovieTitles(Map<String, List<String>> movies){
-        movies.entrySet().stream().flatMap(list -> list.getValue().stream())
-                .forEach(movie -> System.out.print(movie + "!"));
+        String movieTitles = movies.entrySet().stream()
+            .flatMap(entry -> entry.getValue().stream()).collect(Collectors.joining("!"));
+
+       System.out.println(movieTitles);
     }
 }
