da098dea9503b3d9d800f3b18097534e55200aa4
kuba jarosz <kuba.jarooosz@gmail.com>
2022-03-05 19:58:54 +0100

    onlineShop package created

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/movies/MovieStoreMain.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/movies/MovieStoreMain.java
@@ -1,4 +1,4 @@
-package com.kodilla.good.patterns.challenges;
+package com.kodilla.good.patterns.challenges.movies;
 
 public class MovieStoreMain {
     public static void main(String[] args) {
---
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/onlineShop/ProductOrderService.java
@@ -0,0 +1,9 @@
+package com.kodilla.good.patterns.challenges.onlineShop;
+
+public class ProductOrderService {
+    private ProductSercie productService;
+    private InformationService informationService;
+    private StockService stockService;
+    private OrderService orderService;
+
+}
