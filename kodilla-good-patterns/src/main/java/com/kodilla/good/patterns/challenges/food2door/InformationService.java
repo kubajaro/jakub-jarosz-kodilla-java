7481752964f4880f3474602990bf414803ddc73f
kuba jarosz <kuba.jarooosz@gmail.com>
2022-03-09 19:40:51 +0100

    13.4 refactoring

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/food2door/InformationService.java
+++
@@ -1,6 +0,0 @@
-package com.kodilla.good.patterns.challenges.food2door;
-
-public interface InformationService {
-    void orderPlacedInformation();
-    void orderRealizedInformation();
-}
--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/food2door/OrderService.java
+++
@@ -1,6 +0,0 @@
-package com.kodilla.good.patterns.challenges.food2door;
-
-public interface OrderService {
-    void placeOrder(Order order);
-    void realizeOrder(Order order);
-}
