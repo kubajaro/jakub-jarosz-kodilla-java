5068795992c36d54a1e320694fdda5682afd341f
kuba jarosz <kuba.jarooosz@gmail.com>
2022-03-11 23:05:10 +0100

    14.1 polymoprhism

---
+++ kodilla-spring-intro/src/main/java/SpringIntroRunner.java
@@ -0,0 +1,5 @@
+public class SpringIntroRunner {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ kodilla-spring-intro/src/test/java/com/kodilla/spring/intro/shape/DrawerTestSuite.java
@@ -0,0 +1,18 @@
+package com.kodilla.spring.intro.shape;
+
+import org.junit.jupiter.api.Test;
+
+public class DrawerTestSuite {
+
+    @Test
+    void doDrawingTest(){
+        //given
+        Drawer drawer = new Drawer();
+
+        //when
+        drawer.doDrawing();
+
+        //then
+        //nothing
+    }
+}
