4322eabaa27c03e68fae35db6158253bef3d4c6e
kuba jarosz <kuba.jarooosz@gmail.com>
2022-03-13 00:30:59 +0100

    14.1 spring package + shape components

---
+++ kodilla-spring/src/test/java/com/example/kodillaspring/KodillaSpringApplicationTests.java
@@ -0,0 +1,43 @@
+package com.example.kodillaspring;
+
+import com.kodilla.spring.shape.Circle;
+import com.kodilla.spring.shape.Shape;
+import com.kodilla.spring.shape.Triangle;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class KodillaSpringApplicationTests {
+
+    @Test
+    void testCircleLoadedIntoContainer() {
+        //given
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring");
+        Shape shape = context.getBean(Circle.class);
+
+        //when
+        String result = shape.getShapeName();
+
+        //then
+        assertEquals("This is a circle", result);
+    }
+
+
+    @Test
+    void testTriangleLoadedIntoContainer() {
+        //given
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring");
+        Shape shape = context.getBean(Triangle.class);
+
+        //when
+        String result = shape.getShapeName();
+
+        //then
+        assertEquals("This is a triangle", result);
+    }
+
+}
