85884066b61d0f99add89d328bc2077ad7ee55cc
kuba jarosz <kuba.jarooosz@gmail.com>
2022-03-30 21:18:01 +0200

    16.6 task db connection test + query

--- kodilla-jdbc/src/test/java/com/kodilla/jdbc/DbManagerTestSuite.java
+++ kodilla-jdbc/src/test/java/com/kodilla/jdbc/DbManagerTestSuite.java
@@ -10,10 +10,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DbManagerTestSuite {
 
+    //given
+    DbManager dbManager = DbManager.getInstance();
+
     @Test
     void testConnect() throws SQLException {
-        //given
-        DbManager dbManager = DbManager.getInstance();
 
         //when
         String query = "SELECT * FROM USERS";
@@ -31,6 +32,27 @@ public class DbManagerTestSuite {
         rs.close();
         statement.close();
         assertEquals(5, counter);
+    }
+
+    @Test
+    void testSelectUsersAndPosts() throws SQLException {
+        //when
+        String query = "SELECT U.FIRSTNAME, U.LASTNAME, COUNT(*) AS POSTS_NUMBER " +
+                "FROM USERS U JOIN POSTS P ON U.ID = P.USER_ID " +
+                "GROUP BY U.FIRSTNAME, U.LASTNAME " +
+                "HAVING COUNT(*) > 1;";
+        Statement statement = dbManager.getConnection().createStatement();
+        ResultSet rs = statement.executeQuery(query);
+
+        //then
+        int counter = 0;
+        while(rs.next()){
+            System.out.println(rs.getString("FIRSTNAME") + " "
+                    + rs.getString("LASTNAME"));
+            counter++;
+        }
+
+        assertEquals(1, counter);
     }
 
 }
