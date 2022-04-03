6d9f0fc9151fdd318162908964efd947d2c4e899
kuba jarosz <kuba.jarooosz@gmail.com>
2022-04-04 00:04:30 +0200

    17.3 one to many & many to many

--- kodilla-hibernate/src/test/java/com/kodilla/hibernate/tasklist/TaskListDaoTestSuite.java
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/tasklist/TaskListDaoTestSuite.java
@@ -1,13 +1,17 @@
 package com.kodilla.hibernate.tasklist;
 
+import com.kodilla.hibernate.task.Task;
+import com.kodilla.hibernate.task.TaskFinancialDetails;
 import com.kodilla.hibernate.tasklist.dao.TaskListDao;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 public class TaskListDaoTestSuite {
@@ -31,5 +35,28 @@ public class TaskListDaoTestSuite {
 
         //cleanUp
         taskListDao.deleteById(taskListId);
+    }
+
+    @Test
+    void testTaskListDaoSaveWithTasks() {
+        //given
+        Task task = new Task("Test task 1", 14);
+        Task task2 = new Task("Test task 2", 8);
+        TaskFinancialDetails tfd = new TaskFinancialDetails(new BigDecimal(10), false);
+        TaskFinancialDetails tfd2 = new TaskFinancialDetails(new BigDecimal(15), true);
+        task.setTaskFinancialDetails(tfd);
+        task2.setTaskFinancialDetails(tfd2);
+        TaskList taskList = new TaskList("Listname", "ToDoTask");
+        taskList.getTasks().add(task);
+        taskList.getTasks().add(task2);
+        task.setTaskList(taskList);
+        task2.setTaskList(taskList);
+
+        //when
+        taskListDao.save(taskList);
+        int id = taskList.getId();
+
+        //then
+        assertNotNull(id);
     }
 }
