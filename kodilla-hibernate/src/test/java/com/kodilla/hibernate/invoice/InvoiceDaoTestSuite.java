f3a192349dc56ab4657614895dc01db11c963e4f
kuba jarosz <kuba.jarooosz@gmail.com>
2022-04-06 00:36:20 +0200

    17.4 @NamedQueries and @NamedNativeQueries

--- kodilla-hibernate/src/test/java/com/kodilla/hibernate/invoice/InvoiceDaoTestSuite.java
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/invoice/InvoiceDaoTestSuite.java
@@ -1,9 +1,6 @@
 package com.kodilla.hibernate.invoice;
 
 import com.kodilla.hibernate.invoice.dao.InvoiceDao;
-import com.kodilla.hibernate.invoice.dao.ItemDao;
-import com.kodilla.hibernate.invoice.dao.ProductDao;
-import org.assertj.core.api.junit.jupiter.InjectSoftAssertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
